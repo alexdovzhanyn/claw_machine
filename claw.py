eb03c56191255d71bde09f27d87c23b4b44b4129
Alex <dovzhanyn.alex@gmail.com>
2017-09-05 23:39:49 -0400

    x y and z axes should work once implemented

--- claw.py
+++ claw.py
@@ -2,22 +2,27 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 class Claw:
-  def __init__(self, x_direction_pin, x_step_pin, x_movement_direction, y_direction_pin, y_step_pin, y_movement_direction):
-    self.x_direction_pin = x_direction_pin
-    self.x_step_pin = x_step_pin
-    self.y_direction_pin = y_direction_pin
-    self.y_step_pin = y_step_pin
+  def __init__(self, pin_map):
+    self.x_direction_pin = pin_map['x_direction_pin']
+    self.x_step_pin = pin_map['x_step_pin']
+    self.y_direction_pin = pin_map['y_direction_pin']
+    self.y_step_pin = pin_map['y_step_pin']
+    self.z_direction_pin = pin_map['z_direction_pin']
+    self.z_step_pin = pin_map['z_step_pin']
     self.xAxisMoving = False
-    self.yAxisMoving = False
+    self.zAxisMoving = False
     self.delay = 0.001 # How long we wait between GPIO outputs
 
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(x_direction_pin, GPIO.OUT)
-    GPIO.setup(x_step_pin, GPIO.OUT)
-    GPIO.output(x_direction_pin, x_movement_direction) # 1 IS CLOCKWISE, 0 IS COUNTERCLOCKWISE
-    GPIO.setup(y_direction_pin, GPIO.OUT)
-    GPIO.setup(y_step_pin, GPIO.OUT)
-    GPIO.output(y_direction_pin, y_movement_direction)
+    GPIO.setup(self.x_direction_pin, GPIO.OUT)
+    GPIO.setup(self.x_step_pin, GPIO.OUT)
+    GPIO.output(self.x_direction_pin, 1) # 1 IS CLOCKWISE, 0 IS COUNTERCLOCKWISE
+    GPIO.setup(self.y_direction_pin, GPIO.OUT)
+    GPIO.setup(self.y_step_pin, GPIO.OUT)
+    GPIO.output(self.y_direction_pin, 1)
+    GPIO.setup(self.z_direction_pin, GPIO.OUT)
+    GPIO.setup(self.z_step_pin, GPIO.OUT)
+    GPIO.output(self.z_direction_pin, 1)
     
   def toggleMoveLeft(self):
     GPIO.output(self.x_direction_pin, 0)
@@ -26,20 +31,30 @@ class Claw:
   def toggleMoveRight(self):
     GPIO.output(self.x_direction_pin, 1)
     self.xAxisMoving = True
-    
-  def toggleMoveBack(self):
+
+  def toggleMoveUp(self):
     GPIO.output(self.y_direction_pin, 0)
     self.yAxisMoving = True
 
-  def toggleMoveForward(self):
+  def toggleMoveDown(self):
     GPIO.output(self.y_direction_pin, 1)
     self.yAxisMoving = True
+    
+  def toggleMoveBack(self):
+    GPIO.output(self.z_direction_pin, 0)
+    self.zAxisMoving = True
+
+  def toggleMoveForward(self):
+    GPIO.output(self.z_direction_pin, 1)
+    self.zAxisMoving = True
 
   def stopMovementOnAxis(self, axis):
     if axis == 'x':
       self.xAxisMoving = False
     elif axis == 'y':
       self.yAxisMoving = False
+    elif axis == 'z':
+      self.zAxisMoving = False
 
   def updatePosition(self):
     if self.xAxisMoving:
@@ -51,4 +66,9 @@ class Claw:
       GPIO.output(self.y_step_pin, GPIO.HIGH)
       sleep(self.delay)
       GPIO.output(self.y_step_pin, GPIO.LOW)
+      sleep(self.delay)
+    if self.zAxisMoving:
+      GPIO.output(self.z_step_pin, GPIO.HIGH)
+      sleep(self.delay)
+      GPIO.output(self.z_step_pin, GPIO.LOW)
       sleep(self.delay)
--- claw_machine_game.py
+++ claw_machine_game.py
@@ -2,50 +2,57 @@ import pygame
 from claw import Claw
 
 class ClawMachineGame:
+  def __init__(self):
+    pygame.init()
+    self.joystick = pygame.joystick.Joystick(0)
+    self.joystick.init()
+    self.claw = Claw({
+  		x_direction_pin: 23,
+  		x_step_pin: 24,
+  		y_direction_pin: 20,
+  		y_step_pin: 17,
+  		z_direction_pin: 18,
+  		z_step_pin: 21
+  	})
+
+    print 'Initialized Joystick : %s' % self.joystick.get_name()
+    print 'Axes Detected: %s' % self.joystick.get_numaxes()
+    print 'Buttons Detected: %s' % self.joystick.get_numbuttons()
     
-    def __init__(self):
-        pygame.init()
-        self.joystick = pygame.joystick.Joystick(0)
-        self.joystick.init()
-        print 'Initialized Joystick : %s' % self.joystick.get_name()
-        print 'Axes Detected: %s' % self.joystick.get_numaxes()
-        print 'Buttons Detected: %s' % self.joystick.get_numbuttons()
+  def start(self):
+    while True:
+      self.claw.updatePosition()    
+      values = self.getJoyStickValues()
+      print values
         
-        self.claw = Claw(23, 24, 1, 20, 21, 1)
-        
-    def start(self):
-        while True:
-            self.claw.updatePosition()
+      # X AXIS
+      if values[1] < 0:
+        self.claw.toggleMoveLeft()
+      elif values[1] > 0:
+        self.claw.toggleMoveRight()
+      elif values[1] == 0:
+        self.claw.stopMovementOnAxis('x')
             
-            values = self.getJoyStickValues()
-            print values
-            
-            if values[1] < 0:
-                self.claw.toggleMoveLeft()
-            elif values[1] > 0:
-                self.claw.toggleMoveRight()
-            elif values[1] == 0:
-                self.claw.stopMovementOnAxis('x')
+      # Z AXIS
+      if values[0] < 0:
+        self.claw.toggleMoveBack()
+      elif values[0] > 0:
+        self.claw.toggleMoveForward()
+      elif values[0] == 0:
+        self.claw.stopMovementOnAxis('z')
                 
-            if values[0] < 0:
-                self.claw.toggleMoveBack()
-            elif values[0] > 0:
-                self.claw.toggleMoveForward()
-            elif values[0] == 0:
-                self.claw.stopMovementOnAxis('y')
-                
-    def getJoyStickValues(self):
-        out = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        it = 0 #iterator
-        pygame.event.pump()
-        
-        #Read input from the two joysticks       
-        for i in range(0, self.joystick.get_numaxes()):
-            out[it] = self.joystick.get_axis(i)
-            it+=1
-        #Read input from buttons
-        for i in range(0, self.joystick.get_numbuttons()):
-            out[it] = self.joystick.get_button(i)
-            it+=1
-        return out
+  def getJoyStickValues(self):
+    out = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    it = 0 #iterator
+    pygame.event.pump()
+    
+    #Read input from the two joysticks       
+    for i in range(0, self.joystick.get_numaxes()):
+      out[it] = self.joystick.get_axis(i)
+      it+=1
+    #Read input from buttons
+    for i in range(0, self.joystick.get_numbuttons()):
+      out[it] = self.joystick.get_button(i)
+      it+=1
+    return out
     
